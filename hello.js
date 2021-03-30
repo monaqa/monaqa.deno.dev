620da667b23b2e4ca320f2ea3b9624e07948f863
monaqa <mogassy@yahoo.co.jp>
2021-03-30 17:42:39 +0900

    update

--- hello.js
+++ hello.js
@@ -1,6 +1,11 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+  event.respondWith(
+    new Response("Hello world", {
+      status: 200,
+      headers: {
+        server: "denosr",
+        "content-type": "text/plain",
+      },
+    }),
+  );
 });
